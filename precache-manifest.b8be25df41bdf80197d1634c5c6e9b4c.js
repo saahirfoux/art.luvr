113737a66528d9bbca07a84797b17684d5a49820
saahirfoux <saahirhfoux@gmail.com>
2020-09-29 16:04:50 -0400

    Updates

---
+++ precache-manifest.b8be25df41bdf80197d1634c5c6e9b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4aee477f257bb96719d8a3aeafb25f",
+    "url": "/art.luvr/index.html"
+  },
+  {
+    "revision": "6cf4323d6fceeb8069fe",
+    "url": "/art.luvr/static/css/main.e9fa643f.chunk.css"
+  },
+  {
+    "revision": "3354ce49dc17dd0b31ef",
+    "url": "/art.luvr/static/js/2.46621b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/art.luvr/static/js/2.46621b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf4323d6fceeb8069fe",
+    "url": "/art.luvr/static/js/main.8dfac6d9.chunk.js"
+  },
+  {
+    "revision": "6e7d083a22455d2a414b",
+    "url": "/art.luvr/static/js/runtime-main.eecbc0dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/art.luvr/static/media/logo.5d5d9eef.svg"
+  }
+]);
